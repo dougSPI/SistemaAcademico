28560318ac9c8253e587bcf0f1bb1b9f9007da40
dougSPI <profdouglasnunes@gmail.com>
2017-05-26 23:59:48 -0300

    Front-End

---
+++ principal.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Principal</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <img src="img/topo.png" alt="topo" class="img-responsive">
+      </div>  
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <ul class="nav nav-tabs nav-justified menu" role="tablist">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Matérias</a></li>
+          <li><a href="#">adicionar</a></li>
+          <li><a href="#">Sair</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+    <div class="container">
+        <div class="row">
+          <div class="table-responsive">
+            <div class="col-xs-6">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Disciplina</th>
+                    <th>Professor</th>
+                    <th>Notas</th> 
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>PHP</td>
+                    <td>Daniel Brandão</td>
+                    <td>4,7</td>
+                  </tr>
+                  <tr>
+                    <td>PHP</td>
+                    <td>Daniel Brandão</td>
+                    <td>4,7</td>
+                  </tr>
+                  <tr>
+                    <td>PHP</td>
+                    <td>Daniel Brandão</td>
+                    <td>4,7</td>
+                  </tr>
+                  <tr>
+                    <td>PHP</td>
+                    <td>Daniel Brandão</td>
+                    <td>4,7</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <hr>
+
+          <div class="table-responsive">
+            <div class="col-xs-6">
+              <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>Disciplina</th>
+                  <th>Falta por Estágio</th>
+                  <th>total de faltas</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>PHP</td>
+                  <td>4</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>PHP</td>
+                  <td>5</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>PHP</td>
+                  <td>8</td>
+                  <td>10</td>
+                </tr>
+                <tr>
+                  <td>PHP</td>
+                  <td>7</td>
+                  <td>10</td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
+        </div>
+      </div>
+  </div>
+  </div>
+  </div>
+</body>
+</html>
